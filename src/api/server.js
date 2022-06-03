237a25ffe119a389babf8bbbba5aa052b6e73cf3
Alana Correia <alanacorreia.ac@gmail.com>
2022-06-02 22:55:31 -0300

    adiciona cors

--- src/api/server.js
+++ src/api/server.js
@@ -2,4 +2,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+const cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+})
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
